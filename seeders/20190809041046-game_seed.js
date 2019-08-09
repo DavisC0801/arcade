b685c16d78be2bbae010ba9f337f52c8afd3a1ce
davisc0801 <davisc1014@gmail.com>
2019-08-08 22:29:01 -0600

    Add arcade game seeds

---
+++ seeders/20190809041046-game_seed.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Games', [{
+      title: "Tron",
+      price: 100,
+      releaseYear: 1982,
+      active: true,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      title: "Space Invaders",
+      price: 50,
+      releaseYear: 1978,
+      active: true,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      title: "Street Fighter II",
+      price: 75,
+      releaseYear: 1991,
+      active: false,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      title: "Dance Dance Revolution",
+      price: 125,
+      releaseYear: 1998,
+      active: true,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Games', null, {});
+  }
+};
