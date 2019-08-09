a34b3ac852aad0a7919d706097a57013febc7bfc
davisc0801 <davisc1014@gmail.com>
2019-08-09 14:01:13 -0600

    Add route to read single resource

--- routes/api/v1/games.js
+++ routes/api/v1/games.js
@@ -2,7 +2,19 @@ var express = require("express");
 var router = express.Router();
 var game = require('../../../models').Game;
 
-router.get("/", function(req, res, next) {
+router.get("/:id", function(req, res) {
+  game.findByPk(req.params.id)
+    .then(games => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).send(JSON.stringify(games));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({error})
+    });
+});
+
+router.get("/", function(req, res) {
   game.findAll()
     .then(games => {
       res.setHeader("Content-Type", "application/json");
