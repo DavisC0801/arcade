3f82f530525ef8c3bb6d6b7de9036174bbbbff8b
davisc0801 <davisc1014@gmail.com>
2019-08-13 11:38:51 -0600

    Add route to create a store resource

--- routes/api/v1/stores.js
+++ routes/api/v1/stores.js
@@ -2,6 +2,21 @@ var express = require("express");
 var router = express.Router();
 var store = require('../../../models').Store;
 
+router.post('/', function(req, res){
+  store.create({
+    title: req.body.title,
+    phoneNumber: req.body.phoneNumber
+  })
+  .then(store => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(201).send(JSON.stringify(store));
+  })
+  .catch(error => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(500).send({error})
+  });
+});
+
 router.get('/:id', function(req, res){
   store.findByPk(req.params.id)
   .then(store => {
