2e39fa939048721ae4128045db357d8934b564ed
davisc0801 <davisc1014@gmail.com>
2019-08-13 11:42:26 -0600

    Add route to update a store resource

--- routes/api/v1/stores.js
+++ routes/api/v1/stores.js
@@ -2,6 +2,25 @@ var express = require("express");
 var router = express.Router();
 var store = require('../../../models').Store;
 
+router.patch('/:id', function(req, res){
+  store.update({
+    title: req.body.title,
+    phoneNumber: req.body.phoneNumber
+  },{
+    returning: true,
+    where: {
+      id: parseInt(req.params.id)
+  })
+  .then(store => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(202).send(JSON.stringify(store));
+  })
+  .catch(error => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(500).send({error})
+  });
+});
+
 router.post('/', function(req, res){
   store.create({
     title: req.body.title,
